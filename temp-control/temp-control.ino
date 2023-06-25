02f3730bef2e05b4f97ab32bb0c340af9036cb62
nathansrod <nathan.silva.rodrigues@gmail.com>
2023-06-25 15:12:46 -0300

    pump and temp test

--- temp-control/temp-control.ino
+++ temp-control/temp-control.ino
@@ -1,6 +1,9 @@
 #include <DS18B20.h>
 #include <LiquidCrystal_I2C.h>
 
+#define DEBOUNCER_TIMER 5
+#define LCD_REFRESH_TIMER 50
+
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 // Pin alias
@@ -11,13 +14,19 @@ int decTemp = 4;
 int fan = 8;
 int heater = 9;
 int pump = 10;
+
+// Temp sensor initialization
 DS18B20 ds(11);
 
 // Variable declaration
 bool processOn = false;
 int setTemp = 25;
-int measuredTemp = 0;
+float measuredTemp = 0;
 int updateLCDTimer = 0;
+int incButtonDebouncerCounter = 0;
+bool incButtonDebouncerFlag = false;
+int decButtonDebouncerCounter = 0;
+bool decButtonDebouncerFlag = false;
 
 void setup() {
   // Pin setup
@@ -38,6 +47,7 @@ void setup() {
 
 void loop() {
   measuredTemp = ds.getTempC();
+  Serial.println(measuredTemp);
 
   if (measuredTemp > 70) { // Turns process OFF if temp rises above 70
     processOn = false;
@@ -52,47 +62,65 @@ void loop() {
   }
 
   if (digitalRead(incTemp) == LOW) { // Increments temp if incTemp is pressed and temp < 65
-    if (setTemp < 65) {
-      setTemp++;
+    incButtonDebouncerCounter++;
+    if (incButtonDebouncerCounter > DEBOUNCER_TIMER && incButtonDebouncerFlag == false) {
+      if (setTemp < 65) {
+        setTemp++;
+        incButtonDebouncerFlag = true;
+      }
     }
+  }
+  else {
+    incButtonDebouncerCounter = 0;
+    incButtonDebouncerFlag = false;
   }
 
   if (digitalRead(decTemp) == LOW) { // Decreases temp if decTemp is pressed and temp > 25
-    if (setTemp > 25) {
-      setTemp--;
+    decButtonDebouncerCounter++;
+    if (decButtonDebouncerCounter > DEBOUNCER_TIMER && decButtonDebouncerFlag == false) {
+      if (setTemp > 25) {
+        setTemp--;
+        decButtonDebouncerFlag = true;
+      }
     }
+  }
+  else {
+    decButtonDebouncerCounter = 0;
+    decButtonDebouncerFlag = false;
   }
 
   if (processOn) { // Executes control while process is ON
-    
+    digitalWrite(pump, HIGH);
   }
   else { // Turns off heater, and keeps pump and fan on to cool down if temp > 30 and process is OFF
     digitalWrite(heater, LOW);
-    if (measuredTemp > 30) {
-      digitalWrite(fan, HIGH);
-      digitalWrite(pump, HIGH);
-    }
-    else {
-      digitalWrite(fan, LOW);
-      digitalWrite(pump, LOW);
-    }
+    digitalWrite(pump, LOW);
+    // if (measuredTemp > 30) {
+    //   digitalWrite(fan, HIGH);
+    //   digitalWrite(pump, HIGH);
+    // }
+    // else {
+    //   digitalWrite(fan, LOW);
+    //   digitalWrite(pump, LOW);
+    // }
   }
 
-  if (updateLCDTimer == 100) {
+  if (updateLCDTimer == LCD_REFRESH_TIMER) {
     // LCD Update
     updateLCDTimer = 0;
-    lcd.clear();
     lcd.setCursor(0,0);
     if (processOn) {
-      lcd.print("TEMPERATURE CTRL ON");
+      lcd.print("TEMPERATURE CTRL ON ");
     }
     else {
       lcd.print("TEMPERATURE CTRL OFF");
     }
     lcd.setCursor(0,1);
-    lcd.print("TMP(PV/SP)=");
+    lcd.print("PV=");
     lcd.print(measuredTemp);
-    lcd.print("oC/");
+    lcd.print("oC");
+    lcd.setCursor(11,1);
+    lcd.print("SP=");
     lcd.print(setTemp);
     lcd.print("oC");
     lcd.setCursor(0,2);
